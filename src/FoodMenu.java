7d9c399748b4d07684634b51b7bee9848755965e
tonylieu1 <42420454+tonylieu1@users.noreply.github.com>
2020-04-10 04:43:16 -0400

    more of the UI

--- src/FoodMenu.java
+++ src/FoodMenu.java
@@ -12,23 +12,15 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class FoodMenu extends Application {
-	ArrayList<Double> Prices = new ArrayList();
-	ArrayList<String> FoodItems = new ArrayList();
-	public FoodMenu() {
-		
-	}
-	public void addfood(String food) {
-		FoodItems.add(food);
-	}
-	public void addPrice(double price) {
-		Prices.add(price);
-	}
+	CustomorReciepts Receipt = new CustomorReciepts();
+	ArrayList<CustomorReciepts> invocies = new ArrayList();
     public static void main(String[] args) {
         launch(args);
     }
 
     public void start(Stage primaryStage) {
-    	FoodMenu Receipt = new FoodMenu();
+    	//this is where the customor reicepts will be held 
+    	
     	ArrayList<Object> R= new ArrayList();
         TabPane tabPane = new TabPane();
         Button button1 = new Button("Hamburgers & Fries ");
@@ -43,13 +35,48 @@ public class FoodMenu extends Application {
         Button button10 = new Button("Shrimp PoBoy ");
         Button button11 = new Button("Banger and mash ");
         Button button12 = new Button("Salmon and Vegetable medley");
+        Button button13 = new Button("Beef ");
+        Button button14 = new Button("Chicken ");
+        Button button15 = new Button("Shrimp ");
+        Button button16 = new Button("Salmon");
+        Button button17 = new Button("Tuna");
+        Button button18 = new Button("Fries ");
+        Button button19 = new Button("Vegable Medly ");
+        Button button20 = new Button("Chicken Tender ");
+        Button button21 = new Button("Onion Rings");
+        Button button22 = new Button("Rice");
+        Button button23 = new Button("Bean ");
+        Button button24 = new Button("Green Beans ");
+        Button button25 = new Button("Rice");
+        Button button26 = new Button("Fruit");
+        Button button27 = new Button("Honey mustard ");
+        Button button28 = new Button("BBQ sauce ");
+        Button button29 = new Button("Ranch ");
+        Button button30 = new Button("Blue Cheese ");
+        Button button31 = new Button("Bacon ");
+        Button button32 = new Button("Lettuce");
+        Button button33 = new Button("Tomatoes ");
+        Button button34 = new Button("Onions ");
+        Button button35 = new Button("Salad ");
+        Button button36 = new Button("Crotons");   
+        Button button37 = new Button("Add all meats ");
+        Button button38 = new Button("Add all Vegetables ");
+        Button button39 = new Button("Add Beans and Rice with gravity ");
+        Button button40 = new Button("Add Fries and Onions rings ");
+        Button button41 = new Button("Add Greens beans and Mash potatoes ");
+        Button button42 = new Button("Add all sauces ");
+        Button button43 = new Button("Add Green Beans and Vegetable Medley  ");
+        
      
         //this is for the tabs to have a grid sturtures
         GridPane G= new GridPane();
+        GridPane G2 = new GridPane();
         G.setHgap(20);
         G.setVgap(20);
+        G2.setHgap(20);
+        G2.setVgap(20);
         Tab tab1 = new Tab("Food Specials", G);
-        Tab tab2 = new Tab("Sides"  , new Label(""));
+        Tab tab2 = new Tab("Sides"  , G2);
         Tab tab3 = new Tab("Reciept" , new Label(""));
         Tab tab4 = new Tab("Invoices");
         //this is the button to put in the scenes and the row and colums in that order;fv b
@@ -65,6 +92,37 @@ public class FoodMenu extends Application {
         G.add(button10, 1, 2);
         G.add(button11, 2, 2);
         G.add(button12, 3, 2);
+        G2.add(button13, 0,0);
+        G2.add(button14, 1,0);
+        G2.add(button15, 2,0);
+        G2.add(button16, 3,0);
+        G2.add(button17, 0, 1);
+        G2.add(button18, 1, 1);
+        G2.add(button19, 2, 1);
+        G2.add(button20, 3, 1);
+        G2.add(button21, 0, 2);
+        G2.add(button22, 1, 2);
+        G2.add(button23, 2, 2);
+        G2.add(button24, 3, 2);
+        G2.add(button25, 0,3);
+        G2.add(button26, 1,3);
+        G2.add(button27, 2,3);
+        G2.add(button28, 3,3);
+        G2.add(button29, 0, 4);
+        G2.add(button30, 1, 4);
+        G2.add(button31, 2, 4);
+        G2.add(button32, 3, 4);
+        G2.add(button33, 0, 5);
+        G2.add(button34, 1, 5);
+        G2.add(button35, 2, 5);
+        G2.add(button36, 3, 5);
+        G2.add(button37, 0, 6);
+        G2.add(button38, 1, 6);
+        G2.add(button39, 2, 6);
+        G2.add(button40, 3, 6);
+        G2.add(button41, 0, 7);
+        G2.add(button42, 1, 7);
+        G2.add(button43, 2, 7);
         
         tabPane.getTabs().add(tab1);
         tabPane.getTabs().add(tab2);
