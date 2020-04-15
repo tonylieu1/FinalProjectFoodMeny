95801113ffcb6ef64e3f2edf6c6a1de7564de8ce
Tripp <Tripp@DESKTOP-1MJN440>
2020-04-15 18:13:05 -0400

    Added example for facade public methods

---
+++ src/Trip/FacadeExample.java
@@ -0,0 +1,13 @@
+package Trip;
+
+public class FacadeExample {
+
+	public static void main(String[] args) {
+
+		//Example for using facade public methods, put this in a button event or something:
+		Facade facde = new Facade();
+		facde.AddAllMeats();
+		
+	}
+
+}
